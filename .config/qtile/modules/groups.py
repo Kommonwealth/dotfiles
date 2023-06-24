cf1c01bf7b5a3dbbea13c831e9a07ea37acb9a8d
Kommonwealth <vipcopersonal@gmail.com>
2023-06-24 11:26:14 -0400

    Removed most programs 'snapping' to workspaces

--- .config/qtile/modules/groups.py
+++ .config/qtile/modules/groups.py
@@ -5,12 +5,12 @@ from utils.settings import workspace_names
 from modules.keys import keys, mod, shift
 
 workspaces = [
-    {"name": workspace_names[0], "key": "1", "matches": [Match(wm_class="firefox")], "lay": "bsp"},
-    {"name": workspace_names[1], "key": "2", "matches": [Match(wm_class="Sublime Text")], "lay": "monadtall"},
+    {"name": workspace_names[0], "key": "1", "matches": [], "lay": "bsp"},
+    {"name": workspace_names[1], "key": "2", "matches": [], "lay": "bsp"},
     {"name": workspace_names[2], "key": "3", "matches": [], "lay": "bsp"},
-    {"name": workspace_names[3], "key": "4", "matches": [Match(wm_class="discord")], "lay": "bsp"},
-    {"name": workspace_names[4], "key": "5", "matches": [Match(wm_class="Spotify")], "lay": "bsp"},
-    {"name": workspace_names[5], "key": "6", "matches": [Match(wm_class="")], "lay": "bsp"},
+    {"name": workspace_names[3], "key": "4", "matches": [], "lay": "bsp"},
+    {"name": workspace_names[4], "key": "5", "matches": [], "lay": "bsp"},
+    {"name": workspace_names[5], "key": "6", "matches": [], "lay": "bsp"},
     {"name": workspace_names[6], "key": "7", "matches": [Match(wm_class="Steam")], "lay": "bsp"},
 ]
 
